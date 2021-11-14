e3315a61330d5f6b4f3d06afcb691f9abbf17713
srinin01 <58861911+srinin01@users.noreply.github.com>
2021-11-14 04:30:04 -0500

    Update sat_codepipeline.tf

--- sat_codepipeline.tf
+++ sat_codepipeline.tf
@@ -17,7 +17,7 @@ resource "aws_codebuild_project" "sat_proj" {
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
     image                       = "aws/codebuild/amazonlinux2-x86_64-standard:2.0"
-    image_pull_credentials_type = "CODEPIPELINE"
+    image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = false
     type                        = "LINUX_CONTAINER"
   }
