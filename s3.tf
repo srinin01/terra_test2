70a43473c59b655ecc4bedc8fb8a27053825ac02
Srini Personal <srinivasaraonalluri@Srinivasas-MacBook-Pro.local>
2021-11-15 02:25:43 -0500

    ini

--- s3.tf
+++ s3.tf
@@ -6,3 +6,7 @@ resource "aws_s3_bucket" "test_bucket" {
   bucket = "sat-bucket-1-srini"
   acl = "private"
 }
+resource "aws_s3_bucket" "test_bucket2" {
+  bucket = "sat-bucket-2-srini"
+  acl = "private"
+}
